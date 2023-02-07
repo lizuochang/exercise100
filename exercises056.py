f3de615361de0aa29e789aee0abfc6781d8d4c5b
lizuochang <lizuochang@163.com>
2023-02-07 11:05:49 +0800

    exercises sample add

--- exercises056.py
+++ exercises056.py
@@ -21,10 +21,11 @@ if __name__ == '__main__':
 
     canvas = Canvas(width=800, height=600, bg='yellow')
     canvas.pack(expand=YES, fill=BOTH)
+    # canvas.pack(expand=YES, fill=BOTH)
     k = 1
     j = 1
     for i in range(0, 26):
-        canvas.create_oval(310 - k, 250 - k, 310 + k, 250 + k, width=1)
+        canvas.create_oval(400 - k, 300 - k, 400 + k,300 + k, width=1)
         k += j
         j += 0.3
 
